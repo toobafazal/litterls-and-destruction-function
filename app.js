f9226a513c6b66e2794cca27094b0eacae929625
toobafazal12 <tooba.fazal@asinlabs.tech>
2021-09-18 10:28:46 -0700

    assignment

---
+++ app.js
@@ -0,0 +1,37 @@
+// // write a function which returns array. call the function and destructure array values and print the value
+// function foo(){
+//     let array = ['pink','blue','green','black'];
+// }
+
+//  let {[0][1]} = car;
+ console.log(car);
+// Make a car object with model, year, color. display all the values by destructuring
+
+//  let car ={
+//      color:'black',
+//      year: '2020',
+//      model:'cultus'
+//  }
+//  let {color,year,model} = car;
+//  console.log(car);
+
+// use three variable name, greeting and display value using template literals
+
+// let a = "hello";
+// let b = "Tooba";
+// let c ="Good Morning";
+
+//  console.log(`${a} ${b} ${c}`);
+// use array [1,2,3,4,5,6,7,8,9,10] assign first three values in seperate variables and all remaining in one element as an array
+
+// let array1 = [1,2,3];
+// let array2 = [...array1,4,5,6,7,8,9,10];
+//     console.log(array2);
+
+// if car speed is less than 80 display "car speed is slow", if greater than 80 but less than 120 than display 
+// " car speed is moderate" and if greater than 120 display "car speed is fast"
+
+// let speed = 110; 
+// let check = speed < 80 || speed > 120 ? "car speed is slow" : "Car speed is moderate ";
+
+// console.log(check);
